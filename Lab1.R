656de4f1d701f88b87ab45775f123adf2c2b9cad
kazneo <kazneo213@gmail.com>
2023-09-18 14:01:43 -0400

    fixed a issue

--- Lab1.R
+++ Lab1.R
@@ -1,16 +1,21 @@
-EPI_data <- read.csv("./EPI Data/2010EPI_data.csv", header=FALSE)
+install.packages("csv")
+EPI_data = read.csv("EPI Data/2010EPI_data.csv")
+
 #or
-#EPI_data <- read.xlsx(”<path>/2010EPI_data.xlsx")
+#install.packages("xlsx")
+#EPI_data <- read.xlsx("EPI Data/2010EPI_data.xlsx")
 # Note: replace default data frame name – cannot start with numbers!
 View(EPI_data)
-help(View)
 #
 
-attach(EPI_data) 	# sets the ‘default’ object
-fix(EPI_data) 	# launches a simple data editor
-EPI_data 			# prints out values EPI_data$EPI
-tf <- is.na(EPI_data) # records True values if the value is NA
-E <- EPI[!tf] # filters out NA values, new array
+#attach(EPI_data) 	# sets the ‘default’ object
+#fix(EPI_data) 	# launches a simple data editor
+#EPI_data 			# prints out values EPI_data$EPI
+# tf <- is.na(EPI_data) # records True values if the value is NA
+# E <- EPI_data[!tf] # filters out NA values, new array
 
-#summary(EPI)
-#fivenum(EPI,na.rm=TRUE)
+summary(EPI_data)
+fivenum(EPI_data)
+help(fivenum)
+stem(EPI_data)
+

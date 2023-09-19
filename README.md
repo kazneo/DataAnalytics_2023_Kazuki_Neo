c3524f2a7167fb8f5ea913de6be992e975f66a99
kazneo <kazneo213@gmail.com>
2023-09-19 17:32:19 -0400

    added histogram

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DataAnalytics_2023_Kazuki_Neo
+
+This repository is for lab assignments for Fall 2023 Data Analytics class at RPI.
--- lab1_data.R
+++ lab1_data.R
@@ -1,15 +1,25 @@
-EPI_data <- read.csv(”<path>/2010EPI_data.csv")
+EPI_data = read.csv("EPI Data/2010EPI_data.csv", header = FALSE)
+
 #or
-#EPI_data <- read.xlsx(”<path>/2010EPI_data.xlsx")
+install.packages("xlsx")
+EPI_data <- read.xlsx("EPI Data/2010EPI_data.xlsx")
 # Note: replace default data frame name – cannot start with numbers!
 View(EPI_data)
-#
+
+
 attach(EPI_data) 	# sets the ‘default’ object
-fix(EPI_data) 	# launches a simple data editor
-EPI 			# prints out values EPI_data$EPI
-tf <- is.na(EPI) # records True values if the value is NA
-E <- EPI[!tf] # filters out NA values, new array
+fitted(EPI_data) 	# launches a simple data editor
+data.frame <- EPI_data$V14 			# prints out values EPI_data$EPI
+tf <- is.na(data.frame) # records True values if the value is NA
+E <- EPI_data$V14[!tf] # filters out NA values, new array
 
-#other data
-GRUMP_data <- read.csv(”<path>/GPW3_GRUMP_SummaryInformation_2010.csv")
+#summary(E)
+#fivenum(E)
+#stem(E)
 
+str(E)
+
+hist(as.numeric(E),
+     xlab = "EPI", 
+     ylab = "Density", 
+     main = "Histogram of EPI")

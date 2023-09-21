2da8686dd663df4d972e8117a279caec6c9d853b
kazneo <kazneo213@gmail.com>
2023-09-21 10:57:55 -0400

    fixed some issues

--- lab1_data.R
+++ lab1_data.R
@@ -5,26 +5,22 @@ EPI_data = read.csv("EPI Data/2010EPI_data.csv", header = FALSE)
 #EPI_data <- read.xlsx("EPI Data/2010EPI_data.xlsx")
 # Note: replace default data frame name – cannot start with numbers!
 
+View(EPI_data)
 colnames(EPI_data) <- EPI_data[2, ]
 
 # Remove the second row (which is now the header)
 EPI_data <- EPI_data[-2, ]
 
-View(EPI_data)
-
-
-attach(EPI_data) 	# sets the ‘default’ object
+attach(EPI_data)
 fitted(EPI_data) 	# launches a simple data editor
-#data.frame <- EPI_data$V14
-V14
-E <- as.numeric(as.character(V14))
+EPI
+E <- as.numeric(as.character(EPI))
 E <- E[!is.na(E)]
 
-#summary(E)
+summary(E)
 fivenum(as.numeric(E))
 stem(as.numeric(E))
 
-ncol(E)
 # Histogram
 hist(E, seq(30., 95., 1.0), prob=TRUE)
 lines(density(E,na.rm=TRUE,bw=1.))
@@ -34,7 +30,7 @@ plot(ecdf(E), do.points=FALSE, verticals=TRUE)
 
 #QQ
 par(pty="s")
-qqnorm(E); qqline(E)
+qqnorm(E)#; qqline(E)
 
 #QQ fo T Dens
 x<-seq(30,95,1)
@@ -42,3 +38,4 @@ qqplot(qt(ppoints(250), df = 5), x, xlab = "Q-Q plot for t dsn")
 
 
 #Box Plot
+boxplot(E, as.numeric(as.character(DALY)))
